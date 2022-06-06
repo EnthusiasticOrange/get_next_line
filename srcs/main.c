d33c1e7ac174ba8f8ea9b397ef9b6925b8e1aed3
EnthusiasticOrange <airodionov@protonmail.com>
2022-06-06 23:26:16 +0300

    Add example main file to show gnl usage

---
+++ srcs/main.c
@@ -0,0 +1,34 @@
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(int argc, char** argv)
+{
+    int fd = STDIN_FILENO;
+    char* line = NULL;
+	int ret = 0;
+
+    if (argc == 2)
+    {
+        fd = open(argv[1], O_RDONLY);
+        if (fd == -1)
+        {
+            printf("Failed to open input file \"%s\"\n", argv[1]);
+            return 1;
+        }
+    }
+
+    while ((ret = get_next_line(fd, &line)) > 0)
+    {
+        printf("%s\n", line);
+        free(line);
+    }
+
+    if (ret == -1)
+    {
+        printf("Error in get_next_line()\n");
+        return 1;
+    }
+
+    return 0;
+}
